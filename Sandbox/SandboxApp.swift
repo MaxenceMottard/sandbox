0ec322e962aaffd461b579010fa2e822099332a6
Maxence Mottard <mmottard.externe@bedrockstreaming.commmottard.externe@bedrockstreaming.commmottard.externe@bedrock.com>
2024-09-05 11:41:04 +0200

    Initial Commit

---
+++ Sandbox/SandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandboxApp.swift
+//  Sandbox
+//
+//  Created by Maxence Mottard on 05/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct SandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
